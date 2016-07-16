672c7360a19636c78f81009eee66f7338f06b46e
ccortezaguilera <ccortezaguilera@dons.usfca.edu>
2016-07-15 23:28:13 -0700

    Added the background image and current time

--- src/main.c
+++ src/main.c
@@ -1,13 +1,74 @@
 #include <pebble.h>
-
 Window *my_window;
 TextLayer *text_layer;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
+
+
+
+
+static void update_time() {
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+  
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? 
+                                                          "%H:%M" : "%I:%M", tick_time);
+  text_layer_set_text(text_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+    update_time();
+}
+
+static void main_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds  = layer_get_bounds(window_layer);
+  
+  // Create GBitmap
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_TEST);
+
+  // Create BitmapLayer to display the GBitmap
+  s_background_layer = bitmap_layer_create(bounds);
+
+  // Set the bitmap onto the layer and add to the window
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+
+  
+  text_layer = text_layer_create(
+                  GRect(0, PBL_IF_ROUND_ELSE(58,52), bounds.size.w, 50));
+  text_layer_set_background_color(text_layer, GColorClear);
+  text_layer_set_text_color(text_layer, GColorBlack);
+  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy GBitmap
+  gbitmap_destroy(s_background_bitmap);
+
+  // Destroy BitmapLayer
+  bitmap_layer_destroy(s_background_layer);
+
+}
+
 
 void handle_init(void) {
   my_window = window_create();
 
-  text_layer = text_layer_create(GRect(0, 0, 144, 20));
+  window_set_background_color(my_window, GColorBlack);
+
+  window_set_window_handlers(my_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  //text_layer = text_layer_create(GRect(0, 0, 144, 20));
   window_stack_push(my_window, true);
+  update_time();
 }
 
 void handle_deinit(void) {
